2f2953613156a1f6ae296026f4d6607573ebf917
Blaine Morse <blaine.morse.20@cnu.edu>
2022-04-27 15:33:59 -0400

    Initial Commit with Edit Distance already in.

---
+++ AlgorithmAttempts/src/EditDistance.java
@@ -0,0 +1,49 @@
+
+public class EditDistance {
+	private static int[][] distance;
+
+	public static int getEditDistance(String x, String y) {
+		if (!(x instanceof String) || !(y instanceof String))
+			throw new IllegalArgumentException("Both strings must not be null");
+		var n = x.length();
+		var m = y.length();
+		distance = new int[m + 1][n + 1];
+		for (var row = 0; row <= m; row++)
+			distance[row][0] = row;
+		for (var col = 0; col <= n; col++)
+			distance[0][col] = col;
+
+		for (var i = 1; i <= m; i++) {
+			for (var j = 1; j <= n; j++) {
+				distance[i][j] = Math.min(Math.min(distance[i - 1][j] + 1, distance[i][j - 1] + 1),
+						distance[i - 1][j - 1] + (x.charAt(j - 1) == y.charAt(i - 1) ? 0 : 2));
+			}
+		}
+
+//		printEdit(x,y,distance);
+		return distance[m][n];
+	}
+
+	private static void printEdit(String x, String y, int[][] distance) {
+		System.out.println("\t" + String.join("\t", x.split("")));
+		for (var row = 1; row < distance.length; row++) {
+			System.out.print(y.charAt(row - 1));
+			System.out.print("\t");
+			for (var col = 1; col < distance[row].length; col++)
+				System.out.print(distance[row][col] + "\t");
+			System.out.println();
+		}
+	}
+
+	private static void printWholeEdit(String x, String y, int[][] distance) {
+		System.out.println("\t\t" + String.join("\t", x.split("")));
+		for (var row = 0; row < distance.length; row++) {
+			if (row > 0)
+				System.out.print(y.charAt(row - 1));
+			System.out.print("\t");
+			for (var col = 0; col < distance[row].length; col++)
+				System.out.print(distance[row][col] + "\t");
+			System.out.println();
+		}
+	}
+}
---
+++ AlgorithmAttempts/src/EditDistanceDriver.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class EditDistanceDriver {
+	private final static int ALTERNATE_DEPTH = 5;
+
+	public static void main(String[] args) {
+		// Verify command line arguments
+		if (args.length != 1)
+			throw new IllegalArgumentException(
+					"EditDistanceDriver requires a word source file! Program arguements are: <word_source_file>");
+
+		try (Scanner userInput = new Scanner(System.in)) {
+			var knownWordList = getWords(args[0]);
+
+			// Main input-output loop
+			System.out.print("Enter a word or (Q) to quit: ");
+			String inWord = userInput.nextLine();
+			while (!inWord.equals("Q")) {
+				String finalInWord = inWord.toLowerCase();
+				var knownWords = knownWordList.stream()
+						.collect(Collectors.toMap(w -> w, w -> EditDistance.getEditDistance(finalInWord, w)));
+
+				var bestMatch = knownWords.entrySet().stream().min((e1, e2) -> e1.getValue() - e2.getValue());
+
+				if (bestMatch.isEmpty())
+					throw new IllegalArgumentException("There are no words in the known word list!");
+				
+				
+				System.out
+						.println(String.format("Closest match is: %s%nOther close matches include:", bestMatch.get().getKey()));
+
+				for (var i = 0; i < ALTERNATE_DEPTH; i++) {
+					knownWords.remove(bestMatch.get().getKey());
+					bestMatch = knownWords.entrySet().stream().min((e1, e2) -> e1.getValue() - e2.getValue());
+					if (bestMatch.isEmpty())
+						break;
+					else
+						System.out.println(String.format("\t%s", bestMatch.get()));
+				}
+
+				System.out.print("Enter a word or (Q) to quit: ");
+				inWord = userInput.nextLine();
+			}
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		// EditDistance.getEditDistance("graph", "giraffe");
+	}
+
+	private static List<String> getWords(String filename) throws IOException, URISyntaxException {
+		if (filename == null) {
+			throw new IllegalArgumentException("Error: Filename cannot be null!");
+		}
+		var words = getPath(filename);
+		var inFile = words.toFile();
+		if (inFile.isFile() && inFile.getName().endsWith(".txt")) {
+
+			try (var stream = Files.lines(words, StandardCharsets.UTF_8)) {
+				return stream.map(w -> w.toLowerCase()).collect(Collectors.toList());
+			}
+		} else
+			throw new IllegalArgumentException(String.format("Error: The file %s is not a .txt file!", inFile));
+
+	}
+
+	private static Path getPath(String filename) throws URISyntaxException {
+		URL url = EditDistanceDriver.class.getResource(filename);
+		return Path.of(url.toURI());
+	}
+}
